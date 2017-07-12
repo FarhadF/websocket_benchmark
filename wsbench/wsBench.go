1f3c25b8ba8e96f244f27b837e460d6485492588
FarhadF <farhad.farahi@gmail.com>
2017-07-12 09:21:43 +0430

    threadsafe counters

--- wsbench/wsBench.go
+++ wsbench/wsBench.go
@@ -8,6 +8,7 @@ import (
 	_ "os"
 	_ "os/signal"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -16,14 +17,18 @@ func WsBench(address string, path string, sockets int, interval int, message str
 	log.Printf("connecting to %s", u.String())
 	start := time.Now()
 	counter := 0
-	readCounter := 0
-	writeCounter := 0
+	var readCounter uint64 = 0
+	var writeCounter uint64 = 0
 	compareError := 0
 	readError := 0
 	writeError := 0
 	connectionError := 0
 	writeBytes := 0
 	readBytes := 0
+
+	//readChan := make(chan int)
+	//writeChan := make(chan int)
+	//controlChan := make(chan int)
 	var durr time.Duration
 	var wg sync.WaitGroup
 	for {
@@ -47,7 +52,9 @@ func WsBench(address string, path string, sockets int, interval int, message str
 					writeError++
 				} else {
 					writeBytes += len([]byte(message))
-					writeCounter++
+					//writeCounter++
+					atomic.AddUint64(&writeCounter, 1)
+					//writeChan <- 1
 				}
 				_, readMessage, err := co.ReadMessage()
 				if err != nil {
@@ -56,10 +63,14 @@ func WsBench(address string, path string, sockets int, interval int, message str
 				} else if string(readMessage) != message {
 					log.Printf("received message is not the same! recv: %s", readMessage)
 					compareError++
-					readCounter++
+					//readCounter++
+					atomic.AddUint64(&readCounter, 1)
+					//	readChan <- 1
 				} else {
 					readBytes += len(readMessage)
-					readCounter++
+					//readCounter++
+					//	readChan <- 1
+					atomic.AddUint64(&readCounter, 1)
 				}
 				dur := time.Since(writeTime)
 				log.Println(dur)
@@ -69,7 +80,7 @@ func WsBench(address string, path string, sockets int, interval int, message str
 				time.Sleep(time.Duration(interval) * time.Second)
 			}
 			defer wg.Done()
-
+			//controlChan <- 1
 		}()
 
 		//time.Sleep(1 * time.Millisecond)
@@ -77,9 +88,30 @@ func WsBench(address string, path string, sockets int, interval int, message str
 			break
 		}
 	}
+	/*	for i := 0; i < counter; i++ {
+		<-controlChan
+	}*/
+
+	log.Println("here")
+	/*	var readC int
+		var writeC int
+		for i := 0; i < len(readChan); i++ {
+			temp := <-readChan
+			readC += temp
+			log.Println("wtf")
+
+		}
+		for i := 0; i < len(writeChan); i++ {
+			temp := <-writeChan
+			writeC += temp
+		}
+	*/
 	wg.Wait()
+	readCounterF := atomic.LoadUint64(&readCounter)
+	writeCounterF := atomic.LoadUint64(&writeCounter)
 	//	for {
 	//		<-ch
 	//	}
-	log.Println("Total Sent:", writeCounter, "Total Received:", readCounter, "Bytes Sent", writeBytes, "Bytes Received:", readBytes, "Average RTT:", (durr / time.Duration(readCounter)), "Connection Error:", connectionError, "Write Error:", writeError, "Read Error:", readError, "Message Mismatch:", compareError)
+	//log.Println("wtf:", writeC, readC)
+	log.Println("Total Sent:", writeCounterF, "Total Received:", readCounterF, "Bytes Sent", writeBytes, "Bytes Received:", readBytes, "Average RTT:", (durr / time.Duration(readCounter)), "Connection Error:", connectionError, "Write Error:", writeError, "Read Error:", readError, "Message Mismatch:", compareError)
 }
